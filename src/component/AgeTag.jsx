482d58757b5dd236d62d0c2669d40abce882481f
khv2644511 <khv2644511@naver.com>
2023-05-13 02:33:23 +0900

    feat: 마우스 호버 시 이미지 확대로 사용자 인터페이스 개선

--- src/component/AgeTag.jsx
+++ src/component/AgeTag.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function AgeTag({ adult }) {
   return (
     <div
-      className={`absolute top-3 right-3  rounded-full w-9 h-9  flex items-center justify-center ${
+      className={`absolute z-10 top-3 right-3 rounded-full w-9 h-9  flex items-center justify-center ${
         adult ? "bg-red" : "bg-green"
       }`}
     >
--- src/component/MovieImg.jsx
+++ src/component/MovieImg.jsx
@@ -4,12 +4,12 @@ import AgeTag from "./AgeTag";
 export default function MovieImg({ movieInfo, index }) {
   const { poster_path, adult } = movieInfo;
   return (
-    <div className="relative">
-      <span className="absolute left-3 top-1 text-white text-2xl font-bold">
+    <div className="relative overflow-hidden">
+      <span className="absolute left-3 top-1 text-white text-2xl font-bold z-10">
         {index + 1}
       </span>
       <img
-        className="w-50 h-70 object-cover rounded-md border-2 border-grey"
+        className="w-50 h-70 object-cover rounded-md border-2 border-grey hover:scale-105 transition-all"
         src={`https://image.tmdb.org/t/p/original/${poster_path}`}
         alt="영화 포스터"
         loading="lazy"
