2aa886c1b9606cecec1de999b64a8187b7bc4dcf
khv2644511 <khv2644511@naver.com>
2023-05-13 02:50:17 +0900

    feat: 불필요한 코드 제거

--- src/component/MovieList.jsx
+++ src/component/MovieList.jsx
@@ -7,7 +7,6 @@ export default function MovieList({ movieList }) {
       className="grid grid-cols-[minmax(250px,_1fr)_minmax(250px,_1fr)_minmax(250px,_1fr)] lg:grid-cols-4
     "
     >
-      {movieList.length === 0 && <p>리스트가 존재하지 않습니다.</p>}
       {movieList &&
         movieList.map((movieInfo, index) => (
           <MovieItem
