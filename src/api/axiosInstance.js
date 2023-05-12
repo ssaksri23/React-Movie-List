a4ec6e94369b0ffbc948c7a8e11364369f2331ba
khv2644511 <khv2644511@naver.com>
2023-05-12 19:37:40 +0900

    feat: .env 파일 생성 및 api 키 보안

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -5,7 +5,7 @@ export const axiosInstance = axios.create({
   baseURL: BASE_URL,
   headers: { "Content-Type": "application/json" },
   params: {
-    api_key: "c4c2f261766299bd7a173d106c6f7adb",
+    api_key: process.env.REACT_APP_API_KEY,
     language: "ko",
     sort_by: "popularity.desc",
     include_adult: "true",
