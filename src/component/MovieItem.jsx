238820717e02cac0aa2eafc4a5b203ce7912bbd7
khv2644511 <khv2644511@naver.com>
2023-05-12 22:43:08 +0900

    feat: 영화 리스트 클릭 시 상세페이지 이동 기능 구현

--- src/component/MovieItem.jsx
+++ src/component/MovieItem.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import MovieContent from "./MovieContent";
 import MovieImg from "./MovieImg";
+import { useNavigate } from "react-router-dom";
 
-export default function MovieItem({ movieInfo, index }) {
-  //   console.log(movieInfo);
+export default function MovieItem({ movieInfo, index, id }) {
+  const navigate = useNavigate();
 
   return (
-    <li className="flex flex-col px-5 w-full">
+    <li
+      className="flex flex-col px-5 w-full"
+      onClick={() => {
+        navigate(`/details/${id}`, { state: { movieInfo } });
+      }}
+    >
       <MovieImg movieInfo={movieInfo} index={index} />
       <MovieContent movieInfo={movieInfo} />
     </li>
