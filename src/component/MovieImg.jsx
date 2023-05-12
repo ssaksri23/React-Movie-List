7e6a3b362b997acf9ffe10f2d57dc971913ce7e2
khv2644511 <khv2644511@naver.com>
2023-05-13 00:45:14 +0900

    feat: 이미지 최적화를 위한 loading lazy 속성 적용

--- src/component/MovieImg.jsx
+++ src/component/MovieImg.jsx
@@ -9,9 +9,11 @@ export default function MovieImg({ movieInfo, index }) {
         {index}
       </span>
       <img
-        className="w-full h-full object-cover rounded-md border-2 border-grey"
+        className="w-50 h-70 object-cover rounded-md border-2 border-grey"
         src={`https://image.tmdb.org/t/p/original/${poster_path}`}
-        alt=""
+        alt="영화 포스터"
+        loading="lazy"
+        decoding="async"
       />
       <AgeTag adult={adult} />
     </div>
