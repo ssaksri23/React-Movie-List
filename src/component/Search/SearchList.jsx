c03ee72d3fae47baee16cbea08e067df74787874
khv2644511 <khv2644511@naver.com>
2023-05-13 17:42:03 +0900

    feat: 검색 결과 클릭 시 MovieDetail 페이지로 이동되는 기능 추가

--- src/component/Search/SearchList.jsx
+++ src/component/Search/SearchList.jsx
@@ -1,16 +1,28 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function SearchList({ suggestions }) {
+  const navigate = useNavigate();
+  console.log(suggestions.id);
+
+  const handleClick = (e, suggestion) => {
+    e.preventDefault();
+    navigate(`/details/${suggestion.id}`, {
+      state: { movieInfo: suggestion },
+    });
+  };
+
   return (
     suggestions.length > 0 && (
       <ul className="p-2 mt-2 border-2 border-grey z-30 absolute bg-white max-h-96 overflow-y-auto w-96 ">
         {suggestions &&
           suggestions.map((suggestion) => (
-            <li key={suggestion.id} className="pb-2 hover:bg-grey1  ">
-              <button
-                type="submit"
-                className="text-ellipsis overflow-hidden whitespace-nowrap w-full text-left "
-              >
+            <li
+              key={suggestion.id}
+              className="pb-2 hover:bg-grey1"
+              onClick={(e) => handleClick(e, suggestion)}
+            >
+              <button className="text-ellipsis overflow-hidden whitespace-nowrap w-full text-left ">
                 {suggestion.title}
               </button>
             </li>
