83fe7dcbc9c614a786e529a3322d28b2f0891c4d
khv2644511 <khv2644511@naver.com>
2023-05-13 17:47:35 +0900

    feat: NotFound 페이지 마크업

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function NotFound() {
+  return (
+    <div className="">
+      <h1 className="p-20 font-bold text-3xl">NOT FOUND</h1>
+      <div className="text-lg">
+        잘못된 경로로 접속했습니다. url을 확인해주세요!
+      </div>
+    </div>
+  );
+}
