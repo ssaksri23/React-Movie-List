6ee225dfd48be9ba2f111c9681e3161f2973466b
khv2644511 <khv2644511@naver.com>
2023-05-14 09:54:23 +0900

    fix: NavBar active 상태 변경 안되는 문제 해결

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,13 +1,11 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import SearchBar from "./SearchBar";
 
 export default function NavBar() {
-  const [active, setActive] = useState("/boxoffice"); // 현재 활성화된 링크의 경로를 저장하는 상태
+  const location = useLocation("");
+  const { pathname } = location;
 
-  const handleLinkClick = (link) => {
-    setActive(link);
-  };
   return (
     <header className="">
       <div className="flex flex-col ">
@@ -19,15 +17,13 @@ export default function NavBar() {
       <nav className="flex justify-center gap-5 p-5 m-7 text-lg font-bold border-b-2 border-grey1">
         <Link
           to="/boxoffice"
-          onClick={() => handleLinkClick("/boxoffice")}
-          className={`${active === "/boxoffice" ? "text-red" : ""}`}
+          className={`${pathname === "/boxoffice" ? "text-red" : ""}`}
         >
           박스오피스
         </Link>
         <Link
           to="/popular"
-          onClick={() => handleLinkClick("/popular")}
-          className={`${active === "/popular" ? "text-red" : ""}`}
+          className={`${pathname === "/popular" ? "text-red" : ""}`}
         >
           인기 영화
         </Link>
