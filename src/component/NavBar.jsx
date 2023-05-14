e81148ca39d0b6fb43f912664c1ce82ec7e8da97
khv2644511 <khv2644511@naver.com>
2023-05-14 20:01:46 +0900

    feat: SearchBar 컴포넌트 폴더 이동

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
-import SearchBar from "./SearchBar";
+import SearchBar from "./Search/SearchBar";
 
 export default function NavBar() {
   const location = useLocation("");
--- src/component/Search/SearchBar.jsx
+++ src/component/Search/SearchBar.jsx
@@ -1,16 +1,21 @@
 import React from "react";
-import { useInput } from "../hooks/useInput";
+import { useInput } from "../../hooks/useInput";
 import { AiOutlineSearch } from "react-icons/ai";
-import { useDebounceSuggestion } from "../hooks/useDebounceSuggestion";
-import SearchList from "./Search/SearchList";
-import { useClickOutside } from "../hooks/useClickOutside";
+import { useDebounceSuggestion } from "../../hooks/useDebounceSuggestion";
+import SearchList from "./SearchList";
+import { useClickOutside } from "../../hooks/useClickOutside";
+import { useKeyPress } from "../../hooks/useKeyPress";
 
 export default function SearchBar() {
-  const { value, onChange } = useInput("");
+  const { value, onChange, setValue } = useInput("");
   const { suggestions } = useDebounceSuggestion(value);
   const { ref, isVisible, setIsVisible } = useClickOutside();
-
-  const handleSearch = () => {
+  const { focusIndex, setFocusIndex, onKeyDownHandler } = useKeyPress(
+    suggestions,
+    setValue
+  );
+  const handleSearch = (e) => {
+    e.preventDefault();
     setIsVisible(false);
   };
 
@@ -23,10 +28,7 @@ export default function SearchBar() {
     <>
       <form
         ref={ref}
-        onSubmit={(e) => {
-          e.preventDefault();
-          handleSearch();
-        }}
+        onSubmit={handleSearch}
         className="w-96 m-auto overflow-hidden "
       >
         <label htmlFor="search_movie" />
@@ -34,12 +36,17 @@ export default function SearchBar() {
           <input
             value={value}
             onChange={onChangeInput}
+            onFocus={() => {
+              setIsVisible(true);
+              setFocusIndex(0);
+            }}
+            onKeyDown={onKeyDownHandler}
             id="search_movie"
             type="text"
             placeholder="작품, 배우, 감독명을 입력하세요"
             className="w-full p-2 "
           />
-          <button className="absolute right-1 p-3">
+          <button type="submit" className="absolute right-1 p-3">
             <AiOutlineSearch className="w-5 h-5" />
           </button>
         </div>
@@ -47,6 +54,8 @@ export default function SearchBar() {
           setIsVisible={setIsVisible}
           isVisible={isVisible}
           suggestions={suggestions}
+          focusIndex={focusIndex}
+          setFocusIndex={setFocusIndex}
         />
       </form>
     </>
