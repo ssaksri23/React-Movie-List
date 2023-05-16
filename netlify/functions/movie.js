9aeff1a9e13e25ef2695a3477ff1d22a718635f0
khv2644511 <khv2644511@naver.com>
2023-05-17 02:03:09 +0900

    feat: .netlify => netlify폴더로 수정

---
+++ netlify/functions/movie.js
@@ -0,0 +1,27 @@
+// ./functions/movie.js
+const axios = require("axios");
+const urlencode = require("urlencode");
+
+exports.handler = async (event) => {
+  const { queryStringParameters } = event;
+  const parameters = Object.entries(queryStringParameters)
+    .map(([key, value]) => `${key}=${urlencode.encode(value)}`) // *** 인코딩 문제해결
+    .join("&")
+    .concat(`&api_key=${process.env.REACT_APP_API_KEY}`);
+
+  const url = `https://api.themoviedb.org/3/discover/movie?${parameters}`;
+
+  const response = await axios.get(url);
+  const { headers } = response;
+  headers["Access-Control-Allow-Origin"] = process.env.REACT_APP_HOST_SERVER; // *** 동일출처정책 문제해결
+
+  console.log(response.data.results);
+
+  return {
+    statusCode: 200,
+    headers,
+    body: JSON.stringify({
+      data: response.data.results,
+    }),
+  };
+};
