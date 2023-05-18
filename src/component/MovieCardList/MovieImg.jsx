76550094bc69c488c573a5e84a6830f30cc7b97e
khv2644511 <khv2644511@naver.com>
2023-05-19 03:09:10 +0900

    feat: 이미지 최적화를 위한 이미지 크기 지정

--- src/component/MovieCardList/MovieImg.jsx
+++ src/component/MovieCardList/MovieImg.jsx
@@ -4,12 +4,12 @@ import AgeTag from "./AgeTag";
 export default function MovieImg({ movieInfo, index }) {
   const { poster_path, adult } = movieInfo;
   return (
-    <div className="relative overflow-hidden w-50 h-70 rounded-md border-2 border-grey">
+    <div className="relative overflow-hidden w-50 h-75 rounded-md border-2 border-grey">
       <span className="absolute left-3 top-1 text-white text-2xl font-bold z-10">
         {index + 1}
       </span>
       <img
-        className="overflow-hidden hover:scale-105 transition-all"
+        className="overflow-hidden hover:scale-105 transition-all w-50 h-75 "
         src={`https://image.tmdb.org/t/p/original/${poster_path}`}
         alt="영화 포스터"
         loading="lazy"
